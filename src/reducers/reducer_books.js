334cb8d9cfb24cf6cd8fc9bb77f2e221bc178509
Joao Golias <golias.appsimples@gmail.com>
2018-06-22 15:06:49 -0300

    Lesson 2 - Reducers. In this lesson, I have created a function that returns a book list (object with 'title' proprety) and learnt how to import it to index.js (reducers folder main file) using 'combineReducers' from 'redux' lib

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+export default function() {
+    return [
+        { title: 'Harry Potter' },
+
+        { title: 'Mobby Dick' },
+
+        { title: 'The Dark Tower'},
+
+        { title: 'Little Prince'}
+    ]
+}
