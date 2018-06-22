1ac92d610e1d34de7f9c6d262b8b80f3b681204f
Joao Golias <golias.appsimples@gmail.com>
2018-06-22 16:37:27 -0300

    Lesson 4.4, 4.5 and 4.6. In this lesson, I have connected the reducers to the main app component, generating a list of books. Furthermore, I learnt what the reducers do: get status from redux and propagates to the components

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 
 class BookList extends Component{
-    
+
     renderList(){
         return this.props.books.map((book) => 
         {   
@@ -12,13 +13,23 @@ class BookList extends Component{
             )
         })
     }
-
+    
     render(){
+    
         return(
             <ul className='list-group col-sm-4'>
-                {this.renderList}
+                {this.renderList()}
             </ul>
         )
     }
 }
 
+function mapStateToProps(state){
+    //Whatever is returned will show up as props
+    //inside of BookList
+    return {
+        books: state.books
+    }
+}
+
+export default connect(mapStateToProps)(BookList)
